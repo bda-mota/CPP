f40509dd427090f513b9bcaec9f065a002938786
bda-mota <bea.mota04@gmail.com>
2024-09-09 13:27:35 -0300

    first commit

---
+++ 00/ex00/src/megaphone.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string.h>
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	std::string	str;
+
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (i = 1; argv[i] ; i++)
+		{
+			str = argv[i];
+			for (std::string::iterator it = str.begin(); it != str.end(); ++it)
+				*it = static_cast<char>(std::toupper(static_cast<unsigned char>(*it)));
+			std::cout << str;
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
