e46e30d71acc7002fd135f81a2afb8130681f4b7
bda-mota <bea.mota04@gmail.com>
2024-09-16 17:50:11 -0300

    update ex02 -> finished

--- 01/ex02/src/main.cpp
+++ 01/ex02/src/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#define GREEN	"\033[92m"
+#define RED		"\033[91m"
+#define CYAN	"\033[96m"
+#define YELLOW	"\033[93m"
+#define BLUE	"\033[94m"
+#define MAGENT	"\033[95m"
+#define RESET	"\033[0m"
 
 int	main(void)
 {
-	std::string	str = "HI THIS IS BRAIN";
+	std::string	strBrain = "HI THIS IS BRAIN";
+
+	std::string*	stringPTR = &strBrain;
+	std::string&	stringREF = strBrain;
+
+	std::cout << YELLOW << "\n--- Test: addresses ---\n\n" << RESET;
+	std::cout << MAGENT << "Adress of strBrain: " << RESET << &strBrain << std::endl;
+	std::cout << MAGENT << "Adress held by stringPTR: " << RESET << stringPTR << std::endl;
+	std::cout << MAGENT << "Adress held by stringREF: " << RESET << &stringREF << std::endl;
+
+	std::cout << YELLOW << "\n--- Test: values ---\n\n" << RESET;
+	std::cout << MAGENT << "Value of strBrain: " << RESET << strBrain << std::endl;
+	std::cout << MAGENT << "Value pointed to by stringPTR: " << RESET << *stringPTR << std::endl;
+	std::cout << MAGENT << "Value pointed to by stringREF: " << RESET << stringREF << std::endl;
+
+	std::cout << std::endl;
+
+	return (0);
 }
