258dcb936222a8979b996863f2502750d4dbe630
bda-mota <bea.mota04@gmail.com>
2024-09-17 18:52:22 -0300

    atributte args to class

--- 01/ex04/src/main.cpp
+++ 01/ex04/src/main.cpp
@@ -1,40 +1,44 @@
 #include <iostream>
 #include <fstream>
+#include "../includes/Replace.hpp"
 
 int	main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
-	std::ifstream inputFile("text.txt");
-	if (!inputFile) {
-		std::cerr << "Erro ao abrir o arquivo." << std::endl;
-		return 1;
+	if (argc == 4)
+	{
+		std::cout << "the arguments are corrent." << std::endl;
+		Replace	operation(argv[1], argv[2], argv[3]);
+		std::cout << "filename: " << operation.getFilename() << std::endl;
+		std::cout << "S1: " << operation.getS1() << std::endl;
+		std::cout << "S2: " << operation.getS2() << std::endl;
+
 	}
-	std::ofstream outputFile("out.txt");
-    if (!outputFile) {
-        std::cerr << "Erro ao abrir o arquivo para escrita." << std::endl;
-        return 1;
-    }
-
-	std::string line;
-	while (std::getline(inputFile, line)) {
-		outputFile << line << std::endl;
-	}
-
-    if (outputFile.fail()) {
-        std::cerr << "Erro ao gravar no arquivo." << std::endl;
-        return 1;
-    }
-
-	inputFile.close();
-	outputFile.close();
+	else
+		std::cout << "The arguments must be: <filename> s1 s2." << std::endl;
 	return 0;
 }
 
-	// if (argc == 4)
-	// {
-	// 	std::cout << "the arguments are corrent." << std::endl;
+
+	// std::ifstream inputFile("text.txt");
+	// if (!inputFile) {
+	// 	std::cerr << "Erro ao abrir o arquivo." << std::endl;
+	// 	return 1;
 	// }
-	// else
-	// 	std::cout << "The arguments must be: <filename> s1 s2." << std::endl;
-	// return 0;
+	// std::ofstream outputFile("out.txt");
+    // if (!outputFile) {
+    //     std::cerr << "Erro ao abrir o arquivo para escrita." << std::endl;
+    //     return 1;
+    // }
+
+	// std::string line;
+	// while (std::getline(inputFile, line)) {
+	// 	outputFile << line << std::endl;
+	// }
+
+    // if (outputFile.fail()) {
+    //     std::cerr << "Erro ao gravar no arquivo." << std::endl;
+    //     return 1;
+    // }
+
+	// inputFile.close();
+	// outputFile.close();
