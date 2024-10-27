c0c80d49c14895ed6bac351691c3841794855031
bda-mota <bea.mota04@gmail.com>
2024-10-26 21:51:29 -0300

    upload ex00

---
+++ 05/ex00/src/Bureaucrat.cpp
@@ -0,0 +1,60 @@
+#include "../includes/Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat( void ) : _name("Unidentified"), _grade(150) {}
+
+Bureaucrat::Bureaucrat( std::string const name, int grade) : _name(name){
+	if (grade < 1) {
+		throw GradeTooHighException();
+	} else if (grade > 150) {
+		throw GradeTooLowException();
+	}
+	_grade = grade;
+}
+
+Bureaucrat::Bureaucrat( const Bureaucrat& other) {
+	*this = other;
+}
+
+Bureaucrat& Bureaucrat::operator=( const Bureaucrat& other) {
+	const_cast<std::string&>(_name) = other._name;
+		_grade = other._grade;
+	return *this;
+}
+
+Bureaucrat::~Bureaucrat() { }
+
+//Exceptions 
+
+const char* Bureaucrat::GradeTooHighException::what( void ) const throw() {
+	return "Grade too high";
+}
+
+const char* Bureaucrat::GradeTooLowException::what( void ) const throw() {
+	return "Grade too low";
+}
+
+//Getters
+std::string	Bureaucrat::getName( void ) const { return _name; }
+int	Bureaucrat::getGrade( void ) const { return _grade; }
+
+//Setters
+void	Bureaucrat::upGrade() {
+	if (_grade == 1) {
+		throw GradeTooHighException();
+	}
+	_grade--; 
+}
+
+void	Bureaucrat::downGrade() {
+	if (_grade == 150) {
+		throw GradeTooLowException();
+	}
+	_grade++;
+}
+
+//Overload
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& bureaucrat) {
+    os << "Bureaucrat: " << bureaucrat.getName() << ", Grade: " << bureaucrat.getGrade();
+    return os;
+}
+
