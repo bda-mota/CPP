f05ae14cbd9052375e0d1af9b75197d8d18b47f3
bda-mota <bea.mota04@gmail.com>
2024-11-07 18:47:38 -0300

    format

--- 05/ex03/includes/AForm.hpp
+++ 05/ex03/includes/AForm.hpp
@@ -26,25 +26,21 @@ public:
 
 	class GradeTooHighException : public std::exception {
 		public: 
-		
 		virtual const char* what() const throw();
 	};
 
 	class GradeTooLowException : public std::exception {
 		public: 
-		
 		virtual const char* what() const throw();
 	};
 
 	class FormNotSignedException : public std::exception {
 		public: 
-		
 		virtual const char* what() const throw();
 	};
 
 	class FormAlreadySignedException : public std::exception {
 		public: 
-		
 		virtual const char* what() const throw();
 	};
 
