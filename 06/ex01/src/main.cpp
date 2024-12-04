e473c4b39bab3c1b378f49fb6ed4bae7b6ab77a7
bda-mota <bea.mota04@gmail.com>
2024-12-04 19:22:13 -0300

    add mains

--- 06/ex01/src/main.cpp
+++ 06/ex01/src/main.cpp
@@ -1,14 +1,65 @@
-#include "../includes/Data.hpp"
 #include "../includes/Serializer.hpp"
+#include "../includes/Data.hpp"
 
-int main() {
-	Data* data = new Data();
-	uintptr_t raw = Serializer::serialize(data);
-	Data* copy = Serializer::deserialize(raw);
+int main(void)
+{
+	std::cout << BLUE << std::endl;
+	std::cout << "---------------------------------------------------------\n";
+	std::cout << MAGENT;
+	std::cout << "***************** REINTERPRET CAST TEST *****************\n";
+	std::cout << BLUE;
+	std::cout << "---------------------------------------------------------\n";
+	std::cout << CYAN;
+	std::cout << "Creating data instance..." << std::endl;
+	std::cout << "Printing pointer address of data instance..." << std::endl;
+	std::cout << "Printing data informations..." << std::endl;
+	std::cout << BLUE;
+	std::cout << "---------------------------------------------------------\n";
 
-	std::cout << "data->getData(): " << data->getData() << std::endl;
-	std::cout << "copy->getData(): " << copy->getData() << std::endl;
+	Data before(42);
+	std::cout << "Data is in pointer: \t\t" << PURPLE << before << "\n\n";
+	std::cout << before;
 
-	delete data;
-	return (0);
+	std::cout << BLUE << std::endl;
+	std::cout << "---------------------------------------------------------\n";
+	std::cout << CYAN;
+	std::cout << "Creating raw pointer using reinterpret_cast (serializing)\n";
+	std::cout << "Printing raw pointer address..." << std::endl;
+	std::cout << BLUE;
+	std::cout << "---------------------------------------------------------\n";
+
+	uintptr_t raw = Serializer::serialize(before);
+	std::cout << "Raw pointer is: \t\t" << raw << std::endl;
+
+
+	std::cout << BLUE << std::endl;
+	std::cout << "---------------------------------------------------------\n";
+	std::cout << CYAN;
+	std::cout << "Deserializing raw pointer using reinterpret_cast...\n";
+	std::cout << "Confirming that data pointer is still pointing\n";
+	std::cout << "at the same address..." << std::endl;
+	std::cout << "Printing new data informartion..." << std::endl;
+	std::cout << BLUE;
+	std::cout << "---------------------------------------------------------\n";
+
+	Data after = Serializer::deserialize(raw);
+	std::cout << "Data is still pointing at: \t" << PURPLE << after << "\n\n";
+	std::cout << after;
+
+	std::cout << BLUE << std::endl;
+	std::cout << "---------------------------------------------------------\n";
+	std::cout << CYAN;
+	std::cout << "Deleting data instance..." << std::endl;
+	std::cout << BLUE;
+	std::cout << "---------------------------------------------------------\n";
+
+	std::cout << BLUE << std::endl;
+	std::cout << "---------------------------------------------------------\n";
+	std::cout << MAGENT;
+	std::cout << "---------------------- END OF TEST ----------------------\n";
+	std::cout << BLUE;
+	std::cout << "---------------------------------------------------------\n";
+	std::cout << RESET << std::endl;
+
+	return 0;
 }
--- 06/ex02/src/main.cpp
+++ 06/ex02/src/main.cpp
@@ -1,5 +1,59 @@
 #include "../includes/Base.hpp"
 
-int main() {
-	
+int main( void )
+{
+
+	std::cout << BLUE << std::endl;
+	std::cout << "---------------------------------------------------------\n";
+	std::cout << MAGENT;
+	std::cout << "******************* DYNAMIC CAST TEST *******************\n";
+	std::cout << BLUE;
+	std::cout << "---------------------------------------------------------\n";
+
+	 for (int i = 0; i < 10; i++)
+	 {
+		std::cout << CYAN;
+		std::cout << "-> TEST " << i + 1 << std::endl;
+		std::cout << BLUE;
+		std::cout << "---------------------------------------------------------\n";
+		std::cout << YELLOW << "Generating random class...\n" << GREEN ;
+		Base *base = generate();
+
+		std::cout << YELLOW << "\nIdentifying class with pointer...\n" << GREEN;
+		identify(base);
+
+		std::cout << YELLOW << "\nIdentifying class with reference...\n" << GREEN;
+		identify(*base);
+		std::cout << std::endl;
+
+		delete base;
+
+		std::cout << BLUE;
+		std::cout << "---------------------------------------------------------\n";
+	}
+
+	std::cout << "-> TEST 11" << std::endl;
+	std::cout << "---------------------------------------------------------\n";
+	std::cout << YELLOW <<"Generating base class..." << GREEN << std::endl;
+	Base *base = new Base();
+
+	std::cout << YELLOW << "\nIdentifying class with pointer...\n" << GREEN;
+	identify(base);
+
+	std::cout << YELLOW << "\nIdentifying class with reference...\n" << GREEN;
+	identify(*base);
+	std::cout << std::endl;
+
+	delete base;
+
+	std::cout << BLUE << std::endl;
+	std::cout << "---------------------------------------------------------\n";
+	std::cout << MAGENT;
+	std::cout << "---------------------- END OF TEST ----------------------\n";
+	std::cout << BLUE;
+	std::cout << "---------------------------------------------------------\n";
+	std::cout << RESET << std::endl;
+
+	return 0;
 }
+
