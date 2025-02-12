4d97a051cb25943dc5c6a8a62eb7fd3265c5924f
bda-mota <bea.mota04@gmail.com>
2025-02-12 12:11:15 -0300

    add colors

--- 08/ex00/includes/easyfind.hpp
+++ 08/ex00/includes/easyfind.hpp
@@ -1,13 +1,17 @@
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
-#include <iostream>
-#include <vector>
-#include <list>
-#include <set>
-#include <exception>
-#include <algorithm> 
-#include <deque>
+# include <iostream>
+# include <vector>
+# include <list>
+# include <set>
+# include <exception>
+# include <algorithm> 
+# include <deque>
+
+# define CYAN	"\033[96m"
+# define MAGENT	"\033[95m"
+# define RESET   "\033[0m"
 
 template <typename T>
 typename T::const_iterator easyfind(T const &container, int const value) {
--- 08/ex00/src/main.cpp
+++ 08/ex00/src/main.cpp
@@ -4,36 +4,51 @@
 #include <list>
 #include <deque>
 #include <set>
-#include <algorithm>  // para std::find
-#include <exception>  // para std::out_of_range
+#include <algorithm>
+#include <exception> 
 
+
+template <typename T>
+void printContainer(const T& container) {
+	std::cout << CYAN << "Container values: " << RESET;
+    for (typename T::const_iterator it = container.begin(); it != container.end(); ++it) {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+}
 
 int main() {
     try {
+		std::cout << MAGENT " =============== TEST 1: WITH VECTOR =============== " RESET << std::endl;
         std::vector<int> vec;
         vec.push_back(1);
         vec.push_back(2);
         vec.push_back(3);
         vec.push_back(4);
         vec.push_back(5);
-        std::cout << "Searching for 3 in vector: " << *easyfind(vec, 3) << std::endl;
+		printContainer(vec);
+        std::cout << CYAN << "Searching for 3 in vector: " << RESET << *easyfind(vec, 3) << std::endl;
 
+		std::cout << MAGENT " =============== TEST 2: WITH LIST =============== " RESET << std::endl;
         std::list<int> lst;
         lst.push_back(10);
         lst.push_back(20);
         lst.push_back(30);
         lst.push_back(40);
         lst.push_back(50);
-        std::cout << "Searching for 20 in list: " << *easyfind(lst, 20) << std::endl;
+		printContainer(lst);
+        std::cout << CYAN <<"Searching for 20 in list: " << RESET << *easyfind(lst, 20) << std::endl;
 
+		std::cout << MAGENT " =============== TEST 3: WITH DEQUE =============== " RESET << std::endl;
         std::deque<int> deq;
         deq.push_back(100);
         deq.push_back(200);
         deq.push_back(300);
-        std::cout << "Searching for 200 in deque: " << *easyfind(deq, 200) << std::endl;
+		printContainer(deq);
+        std::cout << CYAN <<"Searching for 200 in deque: " << RESET  << *easyfind(deq, 200) << std::endl;
 
-        // Exemplo que gera uma exceção
-        std::cout << "Searching for 99 in vector: " << *easyfind(vec, 99) << std::endl;
+        std::cout << MAGENT " =============== TEST 4: EXAMPLE WITH EXCEPTION =============== " RESET << std::endl;
+        std::cout << CYAN <<"Searching for 99 in vector: " << RESET  << *easyfind(vec, 99) << std::endl;
     }
     catch (const std::exception &e) {
         std::cerr << "Exception: " << e.what() << std::endl;
